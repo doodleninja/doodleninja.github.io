14c2e747fcaeee7a78830becd9b22bd42992dc10
HDSTUDENT <hdstudent@montville.net>
2016-01-25 11:10:23 -0500

    Added levels jsom

---
+++ levels.js
@@ -0,0 +1,50 @@
+levels = [
+    {
+        name: "1",
+        size: 5000
+    },
+    {
+        name: "2",
+        size: 5000
+    },
+    {
+        name: "3",
+        size: 5000
+    },
+    {
+        name: "4",
+        size: 5000
+    },
+    {
+        name: "5",
+        size: 5000
+    },
+    {
+        name: "6",
+        size: 5000
+    },
+    {
+        name: "7",
+        size: 5000
+    },
+    {
+        name: "8",
+        size: 5000
+    },
+    {
+        name: "9",
+        size: 5000
+    },
+    {
+        name: "10",
+        size: 5000
+    },
+    {
+        name: "11",
+        size: 5000
+    },
+    {
+        name: "12",
+        size: 5000
+    }
+]
